732b148df10c1d82f889cb3bc0aee3dc7276e8c6
Youssefanter <yooiialy@gmail.com>
2021-11-03 18:14:16 +0200

    Part Of the GUI

--- MultiQueueSimulation/Form1.Designer.cs
+++ MultiQueueSimulation/Form1.Designer.cs
@@ -28,20 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.serversText = new System.Windows.Forms.TextBox();
+            this.methodText = new System.Windows.Forms.TextBox();
+            this.criteriaText = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.interarrivalTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.probability = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cumulativeProbability = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.range = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of Servers";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Stopping Criteria";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // serversText
+            // 
+            this.serversText.Location = new System.Drawing.Point(109, 21);
+            this.serversText.Name = "serversText";
+            this.serversText.Size = new System.Drawing.Size(23, 20);
+            this.serversText.TabIndex = 3;
+            // 
+            // methodText
+            // 
+            this.methodText.Location = new System.Drawing.Point(109, 47);
+            this.methodText.Name = "methodText";
+            this.methodText.Size = new System.Drawing.Size(23, 20);
+            this.methodText.TabIndex = 4;
+            // 
+            // criteriaText
+            // 
+            this.criteriaText.Location = new System.Drawing.Point(109, 75);
+            this.criteriaText.Name = "criteriaText";
+            this.criteriaText.Size = new System.Drawing.Size(23, 20);
+            this.criteriaText.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Selection Method";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 101);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Stopping Number";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(109, 101);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(23, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.interarrivalTime,
+            this.probability,
+            this.cumulativeProbability,
+            this.range});
+            this.dataGridView1.Location = new System.Drawing.Point(149, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(443, 155);
+            this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // interarrivalTime
+            // 
+            this.interarrivalTime.HeaderText = "Interarrival Time";
+            this.interarrivalTime.Name = "interarrivalTime";
+            // 
+            // probability
+            // 
+            this.probability.HeaderText = "Probability";
+            this.probability.Name = "probability";
+            // 
+            // cumulativeProbability
+            // 
+            this.cumulativeProbability.HeaderText = "Cumulative Probability";
+            this.cumulativeProbability.Name = "cumulativeProbability";
+            // 
+            // range
+            // 
+            this.range.HeaderText = "Range";
+            this.range.Name = "range";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 134);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "proceed";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.ClientSize = new System.Drawing.Size(863, 373);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.criteriaText);
+            this.Controls.Add(this.methodText);
+            this.Controls.Add(this.serversText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox serversText;
+        private System.Windows.Forms.TextBox methodText;
+        private System.Windows.Forms.TextBox criteriaText;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn interarrivalTime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn probability;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cumulativeProbability;
+        private System.Windows.Forms.DataGridViewTextBoxColumn range;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- MultiQueueSimulation/Form1.cs
+++ MultiQueueSimulation/Form1.cs
@@ -14,11 +14,65 @@ namespace MultiQueueSimulation
 {
     public partial class Form1 : Form
     {
+
+        List<DataGridView> tables = new List<DataGridView>();
+
         public Form1()
         {
             InitializeComponent();
         }
-        
-      
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            for (int i = 0; i < int.Parse(serversText.Text); i++)
+            {
+                Form form = new Form();
+                form.Height = 250;
+                form.Width = 450;
+
+                DataGridView table = new DataGridView();
+
+                table.ColumnCount = 4;
+                table.Columns[0].HeaderText = "Service Time";
+                table.Columns[1].HeaderText = "Probability";
+                table.Columns[2].HeaderText = "Cumulative Probability";
+                table.Columns[3].HeaderText = "Range";
+
+                table.Columns[0].Name = "serviceTime";
+                table.Columns[1].Name = "probability";
+                table.Columns[2].Name = "cumulativeProbability";
+                table.Columns[3].Name = "range";
+
+                //Setting Height & Width of table
+                table.Height = 200;
+                table.Width = 440;
+
+                //Add to the "tables" List
+                tables.Add(table);
+
+                // Initialize the form.
+                form.Controls.Add(table);
+                form.AutoSize = true;
+                form.Text = "Server " + (i+1).ToString();
+
+                form.Show();
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
